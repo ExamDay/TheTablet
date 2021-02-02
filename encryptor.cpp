2157e0cf0c1a641193d35b69fa5b1106e32852b1
ExamDay <watch.closely.0@gmail.com>
2021-02-01 23:10:58 -0600

    perfected save load feature!

--- encryptor.cpp
+++ encryptor.cpp
@@ -25,15 +25,49 @@ using CryptoPP::PublicKey;
 using namespace std;
 using namespace CryptoPP;
 
-// void SavePrivateKey(const string& filename, const PrivateKey& key);
-// void SavePublicKey(const string& filename, const PublicKey& key);
-
 void Load(string &filename, BufferedTransformation &bt) {
     FileSource file(filename.c_str(), true /*pumpAll*/);
 
     file.TransferTo(bt);
     bt.MessageEnd();
 }
+
+void keySave(string fileName, RSA::PublicKey key) {
+    // saves public keys to disk into a ".key" file.
+    ByteQueue queue;
+    key.Save(queue);
+    fileName = fileName + ".key";
+    FileSink file(fileName.c_str());
+    queue.CopyTo(file);
+    file.MessageEnd();
+};
+
+void keySave(string fileName, RSA::PrivateKey key) {
+    // saves private keys to disk into a ".key" file.
+    ByteQueue queue;
+    key.Save(queue);
+    fileName = fileName + ".key";
+    FileSink file(fileName.c_str());
+    queue.CopyTo(file);
+    file.MessageEnd();
+};
+
+int keyFileGen(string fileName, size_t keySize) {
+    AutoSeededRandomPool rng;
+    InvertibleRSAFunction inv;
+    inv.GenerateRandomWithKeySize(rng, keySize);
+
+    RSA::PrivateKey privKey(inv);
+    RSA::PublicKey pubKey(inv);
+
+    string pubName = "public_" + fileName;
+    keySave(pubName, pubKey);
+
+    string privName = "private_" + fileName;
+    keySave(privName, privKey);
+
+    return 0;
+};
 
 void LoadPublicKey(string &filename, PublicKey &key) {
     ByteQueue queue;
@@ -48,62 +82,62 @@ void LoadPrivateKey(string &filename, PrivateKey &key) {
 
     key.Load(queue);
 }
-
-void keySave(string fileName, RSA::PublicKey key){
-    // saves public/private key-pairs to disk in ".key" files.
-    ByteQueue queue;
-    key.Save(queue);
-    fileName = fileName + ".key";
-    FileSink file(fileName.c_str());
-    queue.CopyTo(file);
-    file.MessageEnd();
+SecByteBlock charToSecBlock(char *text, size_t textLength) {
+    // copies data from an array of chars into the data region of a SecByteBlock
+    SecByteBlock secbb(textLength);
+    byte *secPtr = secbb.data();
+    for (int i = 0; i < textLength; i++) {
+        memset(secPtr, text[i], 1);
+        secPtr++;
+    };
+    return secbb;
 };
 
-void keySave(string fileName, RSA::PrivateKey key) {
-    // saves public/private key-pairs to disk in ".key" files.
-    ByteQueue queue;
-    key.Save(queue);
-    fileName = fileName + ".key";
-    FileSink file(fileName.c_str());
-    queue.CopyTo(file);
-    file.MessageEnd();
+void saveSecBytes(string filename, SecByteBlock secBlock) {
+    fstream file;
+    cout << filename << endl;
+    file.open(filename, ios::out);
+    file.write((const char *)secBlock.data(), secBlock.size());
+    file.close();
 };
 
-int KeyGen(string fName) {
+SecByteBlock loadSecBytes(string filename) {
+    // loads the bytes of a file (probably containing an RSA encrypted message)
+    // into a SecByteBlock and returns that block.
+    basic_ifstream<byte> file;
+
+    file.open(filename);
+    file.seekg(0, file.end);
+    int fileLength = file.tellg();
+    file.seekg(0, file.beg);
+    file.close();
+
+    SecByteBlock ciphertext(fileLength);
+
+    ByteQueue buffer;
+    Load(filename, buffer);
+
+    cout << "\n\nfile length: " << fileLength << endl;
+    byte *dataptr = ciphertext.data();
+    for (int i = 0; i < fileLength; i++) {
+        cout << buffer[i];
+        memset(dataptr, buffer[i], 1);
+        dataptr++;
+    };
+    buffer.Clear();
+    return ciphertext;
+};
+
+SecByteBlock Encrypt(string keyName, char *inputPlaintext, int plaintextLength) {
 
     AutoSeededRandomPool rng;
-    InvertibleRSAFunction inv;
-    inv.GenerateRandomWithKeySize(rng, 2048);
-
-    RSA::PrivateKey privKey(inv);
-    RSA::PublicKey pubKey(inv);
-
-    // ByteQueue queue;
-    // pubKey.Save(queue);
-    string pubName = "public_" + fName;
-    keySave(pubName, pubKey);
-
-    // queue.Clear();
-    // privKey.Save(queue);
-    string privName = "private_" + fName;
-    keySave(privName, privKey);
-
-    return 0;
-};
-
-SecByteBlock EncrApp(string keyName, char *inputPlaintext, int ptBytes) {
-
-    AutoSeededRandomPool rng;
-
     RSA::PublicKey pubKey;
-
     string pubKeyName = "public_" + keyName + ".key";
-
     LoadPublicKey(pubKeyName, pubKey);
 
-    SecByteBlock plaintext(ptBytes);
+    SecByteBlock plaintext(plaintextLength);
 
-    for (int i = 0; i < ptBytes; i++) {
+    for (int i = 0; i < plaintextLength; i++) {
         plaintext[i] = inputPlaintext[i];
     }
 
@@ -122,107 +156,29 @@ SecByteBlock EncrApp(string keyName, char *inputPlaintext, int ptBytes) {
     return ciphertext;
 }
 
-SecByteBlock DecrApp(string fileName, string keyName) {
-
+SecByteBlock Decrypt(SecByteBlock ciphertext, RSA::PrivateKey privKey) {
     AutoSeededRandomPool rng;
-    string keyFname = "private_" + keyName + ".key";
-    RSA::PrivateKey privKey;
-    LoadPrivateKey(keyFname, privKey);
-
-    basic_ifstream<byte> file;
-
-    file.open(fileName);
-    file.seekg(0, file.end);
-    int fileLength = file.tellg();
-    file.seekg(0, file.beg);
-    file.close();
-
-    SecByteBlock ciphertext(fileLength);
-    // file.read(&ciphertext[0], fileLength);
-    // file.close();
-
-    ByteQueue buffer;
-    Load(fileName, buffer);
-
-    cout << "BUFFER CONTENTS\n\n";
-    byte *dataptr = ciphertext.data();
-    for (int i = 0; i < fileLength; i++) {
-        cout << buffer[i];
-        memset(dataptr, buffer[i], 1);
-        dataptr++;
-    };
-    buffer.Clear();
-    cout << "\n\nWEED\n";
-
     RSAES<OAEP<SHA256>>::Decryptor decryptor(privKey);
 
-    // Now that there is a concrete object, we can check sizes
+    // Check sizes
     assert(0 != decryptor.FixedCiphertextLength());
     assert(ciphertext.size() <= decryptor.FixedCiphertextLength());
 
-    cout << "ciphertext size in bytes: " << ciphertext.size() << endl;
-    size_t declength = decryptor.MaxPlaintextLength(ciphertext.size());
-    // assert (0 != declength);
-    SecByteBlock recovered(declength);
+    // Create recovered text space
+    size_t maxLength = decryptor.MaxPlaintextLength(ciphertext.size());
+    assert(0 != maxLength);
+    SecByteBlock recovered(maxLength);
+    DecodingResult decodeReport = decryptor.Decrypt(rng, ciphertext, ciphertext.size(), recovered);
 
-    DecodingResult result =
-        decryptor.Decrypt(rng, ciphertext, ciphertext.size(), recovered);
+    // More sanity checks
+    assert(decodeReport.isValidCoding);
+    assert(decodeReport.messageLength <= decryptor.MaxPlaintextLength(ciphertext.size()));
 
-    assert(result.isValidCoding);
-    assert(result.messageLength <=
-           decryptor.MaxPlaintextLength(ciphertext.size()));
+    recovered.resize(decodeReport.messageLength);
 
-    recovered.resize(result.messageLength);
-
-    // cout << "\n\nTEST\n" << endl;
-    // const unsigned char *byte = recovered;
-    // int size =result.messageLength;
-    // while (size > 0) {
-    //     size--;
-    //     printf("%c ", *byte);
-    //     byte++;
-    // }
-    // cout << "\n\nEND TEST\n" << endl;
-
-    cout << "\n\nrecovered:\n" << recovered.data() << endl;
+    cout << "\n\nrecovered size:\n" << recovered.size() << endl;
     return recovered;
 };
-
-// SecByteBlock dec(SecByteBlock ciphertext, int size, string keyName) {
-
-//     cout << "\nciphertext: " << ciphertext.data() << endl;
-//     cout << "\nsize: " << size << endl;
-
-//     AutoSeededRandomPool rng;
-
-//     string kname = "private_" + keyName + ".key";
-
-//     RSA::PrivateKey privKey;
-//     LoadPrivateKey(kname, privKey);
-
-//     RSAES<OAEP<SHA256>>::Decryptor decryptor(privKey);
-
-//     size_t declength = decryptor.MaxPlaintextLength(size);
-//     cout << "\ndeclength: " << declength << endl;
-//     SecByteBlock recovered(declength);
-
-//     DecodingResult finout =
-//         decryptor.Decrypt(rng, ciphertext, size, recovered);
-//     cout << "finout valid?: " << finout.isValidCoding << endl;
-//     cout << "finout length:" << finout.messageLength << endl;
-
-//     cout << "\n\nTEST\n" << endl;
-//     unsigned char *item = &recovered[0];
-//     size = finout.messageLength;
-//     while (size > 0) {
-//         size--;
-//         printf("%c ", *item);
-//         item++;
-//     }
-//     cout << "\n\nEND TEST\n" << endl;
-//     // cout << "finout: " << finout.data() << endl;
-//     return recovered;
-// };
 
 int main(int argc, char **argv) {
     int aflag = 0;
@@ -242,14 +198,8 @@ int main(int argc, char **argv) {
 
     opterr = 0;
 
-    while ((c = getopt(argc, argv, ":abd:e:f:g:k:")) != -1)
+    while ((c = getopt(argc, argv, ":d:e:f:g:k:")) != -1)
         switch (c) {
-        case 'a':
-            aflag = 1;
-            break;
-        case 'b':
-            bflag = 1;
-            break;
         case 'd':
             dvalue = optarg;
             dflag = 1;
@@ -270,86 +220,43 @@ int main(int argc, char **argv) {
             kflag = 1;
             break;
         case '?':
-            if (optopt == 'c')
-                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-            else if (isprint(optopt))
+            if (isprint(optopt))
                 fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-            else
-                fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+
             return 1;
+
         default:
             abort();
-        }
+        };
 
-    // decryption code
     if (dflag == 1 && kflag == 1) {
-
-        string ciphertextFileName(dvalue);
-        cout << ciphertextFileName << endl;
-        SecByteBlock recovered = DecrApp(dvalue, kvalue);
+        SecByteBlock ciphertext = loadSecBytes(dvalue);
+        RSA::PrivateKey privKey;
+        string privKeyName = "private_" + (string)kvalue + ".key";
+        LoadPrivateKey(privKeyName, privKey);
+        SecByteBlock recovered = Decrypt(ciphertext, privKey);
         cout << "\n\nrecovered:\n" << recovered.data() << endl;
 
     }
-
     else if (eflag == 1 && kflag == 1 && fflag == 1) {
-
-        int plainBytes = sizeof(evalue);
-        SecByteBlock encodedMessage = EncrApp(kvalue, evalue, plainBytes);
-        cout << encodedMessage.data() << endl;
-        fstream encRes;
-        string encFname = fvalue;
-        encFname = encFname + ".dat";
-        cout << encFname << endl;
-        encRes.open(encFname, ios::out);
-        encRes.write((const char *)encodedMessage.data(),
-                     encodedMessage.size());
-        // encRes << encodedMessage << endl;
-        encRes.close();
-
-        // TEST
-        basic_ifstream<byte> file;
-
-        file.open(encFname);
-        file.seekg(0, file.end);
-        int fileLength = file.tellg();
-        file.seekg(0, file.beg);
-        file.close();
-
-        SecByteBlock ciphertext(fileLength);
-
-        ByteQueue buffer;
-        Load(encFname, buffer);
-
-        cout << "FILE CONTENTS\n\n";
-        byte *dataptr = ciphertext.data();
-        for (int i = 0; i < fileLength; i++) {
-            cout << buffer[i];
-            memset(dataptr, buffer[i], 1);
-            dataptr++;
-        };
-        buffer.Clear();
-
-        cout << "\n\nWEED\n";
-
-        assert(encodedMessage == ciphertext);
-        assert(*encodedMessage.data() == *ciphertext.data());
-        // END TEST
-
+        int plainLength = strlen(evalue);
+        SecByteBlock ciphertext = Encrypt("gabe", evalue, plainLength);
+        cout << "\n\nencodedMessage:\n" << ciphertext.data() << endl;
+        // Save
+        saveSecBytes(fvalue, ciphertext);
     }
-
     else if (gflag == 1) {
-
-        KeyGen(gvalue);
-
+        keyFileGen(gvalue, 2048);
     }
-
     else {
         cout << "Invalid use of program. Terminating." << endl;
         for (index = optind; index < argc; index++) {
             printf("Non-option argument %s\n", argv[index]);
         };
-        abort();
+
+        return 1;
+
     }
 
     return 0;
-}
+};
