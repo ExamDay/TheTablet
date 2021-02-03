7d7db242cbd42c96f87ea3864cc8cc7d3f7ceb7c
lucasrog <lrogers6@wisc.edu>
2021-02-02 21:00:44 -0500

    removed unnecessary couts, slight format change

---
+++ shell.cpp
@@ -0,0 +1,19 @@
+// standard headers:
+#include <cassert>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <sstream>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string>
+#include <typeinfo>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+
+
+
+    return 0;
+}
